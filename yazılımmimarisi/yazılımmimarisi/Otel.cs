8b41b47eaac525e3c4a01a1f0fcbd153ca880cad
Huseyincevik1 <96417296+Huseyincevik1@users.noreply.github.com>
2023-05-25 20:05:42 +0300

    Update Otel.cs

--- yazılımmimarisi/yazılımmimarisi/Otel.cs
+++ yazılımmimarisi/yazılımmimarisi/Otel.cs
@@ -16,7 +16,7 @@ namespace yazılımmimarisi
 
 
 
-        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-1E3PP0A;Initial Catalog=YazılımM;Integrated Security=True");
+        SqlConnection baglanti = new SqlConnection("Data Source=.;Initial Catalog=YazılımM;Integrated Security=True");
         SqlCommand komut;
         public string konaklamaolustur()
         {
